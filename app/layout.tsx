ce9943afe4a51dd243f9c488d6da3c4c3dd680a8
Leo Constantin <lcon69184@gmail.com>
2025-07-14 20:00:27 +0200

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import { siteConfig } from '@/lib/site';
-
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
@@ -34,6 +34,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster closeButton richColors />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
