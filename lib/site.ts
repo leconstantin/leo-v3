1fbd3c77220bced1b66bdc3684f094a7b3b8e090
Leo Constantin <lcon69184@gmail.com>
2025-07-14 20:08:02 +0200

    added opengraph image

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Leo Constantin | Full Stack Developer",
   url: "https://leo-constantin.vercel.app/", // Replace with production URL
-  ogImage: "https://rathon-rw.vercel.app/opengraph-image.png",
+  ogImage: "https://leo-constantin.vercel.app/opengraph-image.png",
   description: "Personal website of Leo Constantin , Full stack Developer.",
   links: {
     twitter: "https://x.com/le_con82546",
